f122abc0ccb9263df01a52ea03d07eca0cba77be
Joseph Mattello <mail@joemattiello.com>
2021-12-31 13:28:06 -0500

    start to edit the js for rom links

--- Resources/content.js
+++ Resources/content.js
@@ -1,5 +1,18 @@
 var handledURL = "";
-var isAutomatic = true;
+var isAutomatic = false;
+
+function matchInArray(string, expressions) {
+    var len = expressions.length,
+        i = 0;
+
+    for (; i < len; i++) {
+        if (string.match(expressions[i])) {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 function runCheck() {
     if (handledURL == window.location.href) {
@@ -7,43 +20,19 @@ function runCheck() {
         return;
     }
 
-    const regexRedditIDs = /^(?:https?:\/\/)?(?:(?:www|amp|m|i)\.)?(?:(?:reddit\.com))\/r\/(\w+)(?:\/comments\/(\w+)(?:\/\w+\/(\w+)(?:\/?.*?[?&]context=(\d+))?)?)?/i;
-    const match = window.location.href.match(regexRedditIDs);
+    const extensions = ["a26", "rom", "nes", "z64", "n64", "sms", "gb"]
+    const match = matchInArray(window.location.href, extensions);
 
     if (match) {
         handledURL = window.location.href;
         window.stop();
 
-        const subreddit = match[1];
-        const postID = match[2];
-        const commentID = match[3];
-        const context = match[4];
+        const downloadURL = window.location.href;
 
-       // postID, commentID, context can be NULL, but subreddit should never be
-        if (postID && commentID && context) {
-            if (isAutomatic) {
-                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}&context=${context}`);
-            } else {
-                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}/?context=${context}`);
-            }
-        } else if (postID && commentID) {
-            if (isAutomatic) {
-                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}`);
-            } else {
-                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}`);
-            }
-        } else if (postID) {
-            if (isAutomatic) {
-                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}`);
-            } else {
-                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}`);
-            }
+        if (isAutomatic) {
+            window.location.replace(`https://openinprovenance.com?downloadURL=${downloadURL}`);
         } else {
-            if (isAutomatic) {
-                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}`);
-            } else {
-                window.location.replace(`provenance://reddit.com/r/${subreddit}`);
-            }
+            window.location.replace(`provenance://downloadURL?${downloadURL}`);
         }
     }
 }

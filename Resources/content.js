fcc276a2960b154c8777e4ba0d2d4be6cd2be4cc
Joseph Mattello <mail@joemattiello.com>
2021-12-31 13:21:33 -0500

    Replace apollo with provenance

--- Resources/content.js
+++ Resources/content.js
@@ -6,7 +6,7 @@ function runCheck() {
         // Already handled, don't want to repeat.
         return;
     }
-    
+
     const regexRedditIDs = /^(?:https?:\/\/)?(?:(?:www|amp|m|i)\.)?(?:(?:reddit\.com))\/r\/(\w+)(?:\/comments\/(\w+)(?:\/\w+\/(\w+)(?:\/?.*?[?&]context=(\d+))?)?)?/i;
     const match = window.location.href.match(regexRedditIDs);
 
@@ -22,27 +22,27 @@ function runCheck() {
        // postID, commentID, context can be NULL, but subreddit should never be
         if (postID && commentID && context) {
             if (isAutomatic) {
-                window.location.replace(`https://openinapollo.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}&context=${context}`);
+                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}&context=${context}`);
             } else {
-                window.location.replace(`apollo://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}/?context=${context}`);
+                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}/?context=${context}`);
             }
         } else if (postID && commentID) {
             if (isAutomatic) {
-                window.location.replace(`https://openinapollo.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}`);
+                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}&commentID=${commentID}`);
             } else {
-                window.location.replace(`apollo://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}`);
+                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}/_/${commentID}`);
             }
         } else if (postID) {
             if (isAutomatic) {
-                window.location.replace(`https://openinapollo.com?subreddit=${subreddit}&postID=${postID}`);
+                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}&postID=${postID}`);
             } else {
-                window.location.replace(`apollo://reddit.com/r/${subreddit}/comments/${postID}`);
+                window.location.replace(`provenance://reddit.com/r/${subreddit}/comments/${postID}`);
             }
         } else {
             if (isAutomatic) {
-                window.location.replace(`https://openinapollo.com?subreddit=${subreddit}`);
+                window.location.replace(`https://openinprovenance.com?subreddit=${subreddit}`);
             } else {
-                window.location.replace(`apollo://reddit.com/r/${subreddit}`);
+                window.location.replace(`provenance://reddit.com/r/${subreddit}`);
             }
         }
     }
